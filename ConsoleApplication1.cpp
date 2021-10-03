ccdc63f68c7316cf0dcf7e9cdb964b8a4382a50b
Maksleader <91394953+Maksleader@users.noreply.github.com>
2021-10-03 16:01:29 +0400

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -2,24 +2,25 @@
 #include <string>
 #include <fstream>
 #include <optional>
-#include <functional>
-#include <tuple>
-#include <map>
 #include <vector>
+#include <sstream>
+#include <iomanip>
+#include <algorithm> 
+#include <tuple>
+#include <stdexcept>
+#include<functional>
+
 
 
 
 using namespace std;
+
 struct Person
 {
 	string name;
 	string surname;
-	string patronymic;
-	optional<string> create(bool patronymic)
-	{
-		return patronymic ? optional<string>{""} : nullopt;
-	}
-
+	string a;
+	optional<string> patronymic;
 
 };
 
@@ -27,21 +28,41 @@ struct Person
 
 bool operator == (struct Person& p1, struct Person& p2)
 {
-	return tie(p1.name, p1.surname, p1.patronymic) == tie(p2.name, p2.surname, p2.patronymic);
+	return tie(p1.surname, p1.name, p1.a) == tie(p2.surname, p2.name, p2.a);
 }
 bool operator < (struct Person& p1, struct Person& p2)
 {
-	return tie(p1.name, p1.surname, p1.patronymic) < tie(p2.name, p2.surname, p2.patronymic);
+	return tie(p1.surname, p1.name, p1.a) < tie(p2.surname, p2.name, p2.a);
 };
 
-
-
-std::ostream& operator<< (std::ostream& s, const Person& p1)
+istream& operator>> (istream& is, Person& p1)
 {
 
-	return s << p1.name << " " << p1.surname << " " << p1.patronymic;
+
+	is >> p1.surname >> p1.name >> p1.a;
+	p1.patronymic = p1.a;
+
+	return is;
 
 
+};
+
+std::ostream& operator<< (std::ostream& s, Person& p1)
+{
+
+	if (p1.patronymic && p1.patronymic == "-")
+	{
+
+		s << right << setw(11) << p1.surname << "        " << right << setw(11) << p1.name << "                      ";
+	}
+	else
+	{
+
+		s << right << setw(11) << p1.surname << "        " << right << setw(11) << p1.name << "        " << right << setw(14) << p1.patronymic.value();
+
+	}
+
+	return s;
 };
 
 
@@ -50,65 +71,153 @@ struct PhoneNumber
 	int country_code{};
 	int city_code{};
 	string number;
-	int additional_number{};
-	optional<int> creat(bool additional_number)
-	{
-		return additional_number ? optional<int>{} : nullopt;
-	}
+	int b{};
+	optional<int>additional_number;
+
+
+
 };
 
-std::ostream& operator<< (std::ostream& out,  PhoneNumber& p1)
+
+bool operator == (struct PhoneNumber& p1, struct PhoneNumber& p2)
+{
+	return tie(p1.country_code, p1.city_code, p1.b) == tie(p2.country_code, p2.city_code, p2.b);
+}
+bool operator < (struct PhoneNumber& p1, struct PhoneNumber& p2)
+{
+	return  tie(p1.country_code, p1.city_code,p1.number, p1.b) < tie(p2.country_code, p2.city_code,p2.number, p2.b);
+}
+
+
+istream& operator>> (istream& is, PhoneNumber& p1)
 {
 
+	is >> p1.country_code >> p1.city_code >> p1.number >> p1.b;
+	p1.additional_number = p1.b;
 
-
-	return out << "+" << p1.country_code << "(" << p1.city_code << ")" << p1.number << " " << p1.additional_number;
-
+	return is;
 };
+
+
+std::ostream& operator<< (std::ostream& out, PhoneNumber& p1)
+{
+	if (p1.additional_number && p1.additional_number == 0)
+	{
+		out << "+" << p1.country_code << "(" << p1.city_code << ")" << p1.number << " ";
+	}
+	else
+	{
+		out << "+" << p1.country_code << "(" << p1.city_code << ")" << p1.number << " " << p1.additional_number.value();
+	}
+	return out;
+};
+bool Compare(pair<Person, PhoneNumber> phonebook1, pair<Person, PhoneNumber> phonebook2)
+{
+	return phonebook1.first < phonebook2.first;
+};
+bool Compare1(pair<Person, PhoneNumber> phonebook1, pair<Person, PhoneNumber> phonebook2)
+{
+	return phonebook1.second < phonebook2.second;
+};
+
+
+
+
 
 class Phonebook
 {
 public:
-	vector<pair<Person, PhoneNumber>>myphonebook;
+	vector<pair<Person, PhoneNumber>>phonebook;
 
 public:
+
 	Phonebook(ifstream& file)
 	{
 
 		Person person;
 		PhoneNumber number;
-		
-		
+		string line;
+
 		
 
 		if (file.is_open())
 		{
-			
+
 			while (!file.eof())
 			{
-				file >> person.name >> person.surname >> person.patronymic>> number.country_code >>number.city_code>>number.number>>number.additional_number;
-				
-				
-					myphonebook.push_back(make_pair(person, number));
+
+
+				getline(file, line);
+				stringstream geek(line);
+
+				geek >>person>> number;
+
+				phonebook.push_back(make_pair(person, number));
 			}
+
+				file.close();
 			
-			file.close();
-			
-		}
+		};
 		
+		
+	};
+	
+	void SortByName()
+	{
+		sort(phonebook.begin(), phonebook.end(), Compare);
+	
 	}
 
-};
-ostream& operator<< (ostream& print, Phonebook& phonebook)
-{
-	
-	for (auto i = 0; i < phonebook.myphonebook.size(); i++)
+	void SortByPhone()
 	{
-		print << phonebook.myphonebook[i].first<< phonebook.myphonebook[i].second;
+		sort(phonebook.begin(), phonebook.end(), Compare1);
+
 	}
+	string foo()
+	{
+		string a, b, c, d;
+		if (a == b)
+		{
+			return c;
+		}
+		else
+		{
+			return c = "not found";
+		}
+	}
+	
+	auto GetPhoneNumber(string surname)
+	{
+		
+		string b;
+		PhoneNumber number;
+	
+
+		
+
+		for_each(phonebook.begin(), phonebook.end(), [surname,&b](const auto& phonebook)
+		{
+				
+		});
+
+		return make_tuple (b,number);
+
+	};
+	
+};
+
+
+
+ostream& operator<< (ostream& print, Phonebook& vec)
+{
+	for ( auto&[Person,PhoneNumber]:vec.phonebook )
+	{
+		print<< Person << right << setw(12) <<PhoneNumber << endl;
+	};
+
+
 	return print;
 }
-
 
 
 
@@ -120,8 +229,27 @@ int main()
 	ifstream file("Input.txt");
 	Phonebook book(file);
 	cout << book;
+	cout << "------SortByPhone-------" << endl;
+	book.SortByPhone();
+	cout << book;
+	cout << "------SortByName--------" << endl;
+	book.SortByName();
+	cout << book;
+	cout << "-----GetPhoneNumber-----" << endl;
+	auto print_phone_number = [&book](const string&surname)
+	{
+		cout << surname << "\t";
+		auto answer = book.GetPhoneNumber(surname);
+		if (get<0>(answer).empty())
+			cout << get<1>(answer);
+		else
+			cout << get<0>(answer);
+		cout << endl;
+	};
 
+	print_phone_number("Ivanov");
 
+	print_phone_number("Petrov");
 
 }
 
